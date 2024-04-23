3fdf919ffd41a507362a4d056f7240cddeb7c7b8
Max Wilkinson <mjswilkinson@outlook.com>
2024-04-23 23:01:49 +0100

    finished popup message upon correct and incorrect answers in cognitive.

--- src/healthapp/popup_window.py
+++
@@ -1,4 +0,0 @@
-class PopupWindow:
-    # implement popup window (current errors)
-    def show_message(window, message):
-        window.info_dialog('Message', message)
--- src/healthapp/windows/cognitive_spell.py
+++ src/healthapp/windows/cognitive_spell.py
@@ -7,7 +7,6 @@ from healthapp.app import HealthApp
 from healthapp.style import create_border
 
 from healthapp.windows.choice_menu import ChoiceMenu as cm
-from healthapp.popup_window import PopupWindow
 #-------------------------------------------------------------------------------------------------------#
 class CognitiveSpell():
     def __init__(self, app: HealthApp):  
@@ -56,20 +55,20 @@ class CognitiveSpell():
     def choice1_handler(self, widget):
         print("Choice 1 button pressed! (Incorrect)")
         print(f"Cognitive Score: {cm.cognitive_score}")
-        # PopupWindow.show_message(self.app, "Incorrect!")
+        self.app.main_window.info_dialog("Incorrect Answer!", "You have answered incorrectly! \nYou have been returned to the Cognitive Test Menu.")
         self.app.show_cognitive()
     
     def choice2_handler(self, widget):
         print("Choice 2 button pressed! (Correct)")
         cm.increment_score()
         print(f"Cognitive Score: {cm.cognitive_score}")
-        # PopupWindow.show_message(self.app, "Correct!")
+        self.app.main_window.info_dialog("Correct Answer!", "You have answered correctly! \nYou have been returned to the Cognitive Test Menu.")
         self.app.show_cognitive()
 
     def choice3_handler(self, widget):
         print("Choice 3 button pressed! (Incorrect)")
         print(f"Cognitive Score: {cm.cognitive_score}")
-        # PopupWindow.show_message(self.app, "Incorrect!")
+        self.app.main_window.info_dialog("Incorrect Answer!", "You have answered incorrectly! \nYou have been returned to the Cognitive Test Menu.")
         self.app.show_cognitive()
     
     def cs_class_handler(self, widget):
