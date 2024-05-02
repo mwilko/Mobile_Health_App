fd1bbea734fad0c9fd7b5b2d10e4dea590c82621
Jack Honour <JaxkDev@gmail.com>
2024-05-02 18:24:21 +0100

    Fix the diabetes page

--- src/healthapp/windows/mla_diabetes.py
+++ src/healthapp/windows/mla_diabetes.py
@@ -33,25 +33,25 @@ class Diabetes():
             color='black', background_color="#fbf5cc", padding=(-3)))
         back_box = create_border(back_button, inner_color="#fbf5cc")
 
-        # label + button for stroke
-        stroke_label = toga.Label("Have you had a stroke?", style=Pack(
+        # label + button for diabetes
+        diabetes_label = toga.Label("Do you have diabetes?", style=Pack(
             color='black', font_size=15, padding=(0, 5)))
 
-        yesstroke_button = toga.Button('Yes', on_press=self.stroke_handler, style=Pack(
+        yes_button = toga.Button('Yes', on_press=self.diabetes_handler, style=Pack(
             color='black', background_color="#fbf5cc", padding=(-3)))
-        yesstroke_box = create_border(
-            yesstroke_button, inner_color="#fbf5cc")
+        yes_box = create_border(
+            yes_button, inner_color="#fbf5cc")
 
-        nostroke_button = toga.Button('No', on_press=self.stroke_handler, style=Pack(
+        no_button = toga.Button('No', on_press=self.diabetes_handler, style=Pack(
             color='black', background_color="#fbf5cc", padding=(-3)))
-        nostroke_box = create_border(
-            nostroke_button, inner_color="#fbf5cc")
+        no_box = create_border(
+            no_button, inner_color="#fbf5cc")
 
         # adding labels to page
-        header_box.add(stroke_label)
+        header_box.add(diabetes_label)
 
         main_box.add(toga.Label(""))
-        for button in [yesstroke_box, nostroke_box, back_box]:
+        for button in [yes_box, no_box, back_box]:
             if button != back_box:
                 main_box.add(button)
                 main_box.add(toga.Label(""))
@@ -65,9 +65,9 @@ class Diabetes():
 
         return content
 
-    def stroke_handler(self, widget):
-        # Update the stroke variable based on the user's selection
-        self.app.user.stroke = (1 if widget.text == 'Yes' else 0)
+    def diabetes_handler(self, widget):
+        # Update the diabetes variable based on the user's selection
+        self.app.user.diabetes = (1 if widget.text == 'Yes' else 0)
         self.app.user.save()
 
     def back_handler(self, widget):
