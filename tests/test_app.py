db986490273a4dc66584ee4448ac4304a2e5540f
Jack Honour <JaxkDev@gmail.com>
2024-05-03 16:54:44 +0100

    Very basic ML pytest
    Use:
    
    briefcase dev --test -r

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,37 @@
-def test_first():
-    "An initial test for the app"
-    assert 1 + 1 == 2
+from pathlib import Path
+import pytest
+
+class Struct:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+def test_mla_valid():
+    "Test the MLA perform_prediction using demo app"
+    
+    # Since the app is not running, we need to mock the app object
+    app = {
+        "paths": Struct(**{
+            "app": Path("src/healthapp")
+        })
+    }
+
+    input_data = [[1, 1, 100, 1, 1, 1, 1, 1, 1, 1, 1, 70]] # Sample input data
+    from src.healthapp.machine_learning import perform_prediction
+    prediction = perform_prediction(Struct(**app), input_data)
+    assert prediction is not None
+    assert prediction >= 0 and prediction <= 100
+
+def test_mla_invalid():
+    # Since the app is not running, we need to mock the app object
+    "Test the MLA perform_prediction using demo app"
+    
+    # Since the app is not running, we need to mock the app object
+    app = {
+        "paths": Struct(**{
+            "app": Path("src/healthapp")
+        })
+    }
+
+    input_data = [[1, 1, 100, 1, 1, 1, None, 1, 1, 1, 1, 70]] # Sample input data
+    from src.healthapp.machine_learning import perform_prediction
+    pytest.raises(ValueError, perform_prediction, Struct(**app), input_data)
